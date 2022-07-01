3137f2b0f015e949bce08ad2870aaf169d0d2ace
Akshaya <akshaya.c@webandcrafts.in>
2022-07-01 18:29:11 +0530

    login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,9 +2,9 @@
     <div class="col-md-12">
         <div class="col-md-4"><a href="{{ route('student.login')}}" >Student Login</a></div><br/>
 
-        <div class="col-md-4"><a href="" >Admin Login</a></div><br/>
+        <div class="col-md-4"><a href="{{ route('login')}}" >Admin Login</a></div><br/>
 
-        <div class="col-md-4"><a href="" >Teacher Login</a></div><br/>
+        <div class="col-md-4"><a href="{{ route('login')}}" >Teacher Login</a></div><br/>
     </div>
 
 </div>
